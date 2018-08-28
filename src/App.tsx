05bd10b61e4ff897564c7d06e89faf8652741d88
Mei Qingguang <meiqg@meiqg-mac.local>
2018-08-28 11:14:56 +0800

    videojs demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
 import * as React from 'react';
-import './App.css';
+import videojs from 'video.js';
 
-import logo from './logo.svg';
+import 'video.js/dist/video-js.css';
 
 class App extends React.Component {
+  private player?: videojs.Player;
+  private videoNode?: HTMLVideoElement;
+
+  constructor() {
+    super({});
+    this.player = undefined;
+    this.videoNode = undefined;
+  }
+  componentDidMount() {
+    this.player = videojs(this.videoNode, {
+      autoplay: false,
+      controls: true,
+      sources: [
+        {
+          src: 'http://vjs.zencdn.net/v/oceans.mp4',
+          type: 'video/mp4',
+        },
+      ],
+    }).ready(function onReady() {
+      // console.log('onPlayerReady', this);
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div data-vjs-player>
+          <video
+            ref={(node: HTMLVideoElement) => (this.videoNode = node)}
+            className="video-js"
+          />
+        </div>
       </div>
     );
   }
